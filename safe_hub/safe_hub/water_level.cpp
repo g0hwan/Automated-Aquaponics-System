93dfe32ebdd313fb532bdb0ad4fad4153c55c5c0
g0hwan <gyeon0614@gmail.com>
2026-01-05 06:16:30 +0900

    01.05 06:16 안전허브 완료

--- safe_hub/safe_hub/water_level.cpp
+++ safe_hub/safe_hub/water_level.cpp
@@ -1,7 +1,14 @@
 #include "water_level.h"
+#include "emergency.h"
+#include "pump_motor.h"
 
 const int wl_pin[2] = {A0,A1};
-const uint8_t buz_pin = 5;
+const uint8_t buz_pin = 8;
+
+int rawval1;
+int rawval2;
+float wl_mm1;
+float wl_mm2;
 
 void lev_pin()
 {
@@ -9,12 +16,13 @@ void lev_pin()
 }
 void lev()
 {
-  int rawval1 = analogRead(wl_pin[0]);
-  float wl_mm1 = 42.0 + ((rawval1 - 722.0) * 288.0 / (1022.0 - 722.0));
-  Serial.print("Level(mm): ");
-  Serial.print(wl_mm1, 1);
-
-  if (wl_mm1 < 150) digitalWrite(buz_pin, HIGH);
-  else digitalWrite(buz_pin, LOW);
-  delay(1000);  // 1초 주기
+  rawval1 = analogRead(wl_pin[0]);
+  rawval2 = analogRead(wl_pin[1]);
+  wl_mm1 = 42.0 + ((rawval1 - 722.0) * 288.0 / (1022.0 - 722.0));
+  wl_mm2 = 42.0 + ((rawval2 - 722.0) * 288.0 / (1022.0 - 722.0));  
+  Serial.print("Level1(mm): ");
+  Serial.println(wl_mm1, 1);
+  Serial.print("Level2(mm): ");
+  Serial.println(wl_mm2, 1);
+  delay(200);  // 1초 주기
 }
