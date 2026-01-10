ea75001eadb1ea52231daff911737c171a35d2d6
g0hwan <gyeon0614@gmail.com>
2026-01-11 04:23:14 +0900

    01.11 04:22 뭐 추가했더라?

--- stepmotor_test/scara_pid/scara_pid.ino
+++ stepmotor_test/scara_pid/scara_pid.ino
@@ -1,11 +1,8 @@
 #include "set_motor.h"
 #include "encoder.h"
 #include "pid.h"
-//#include "kinematic.h"
-
-unsigned long t1=0, t2=0, t3=0, t4=0;
-const unsigned long PID_DT_MS = 10; 
-
+#include "move.h"
+ 
 void setup() {
   Serial.begin(115200);
   motor_pin();
@@ -15,8 +12,5 @@ void setup() {
 
 void loop() {
   
-  unsigned long now = millis();
-  if (now - t1 >= PID_DT_MS) { t1 = now; move_j1(-1000); }
-  if (now - t3 >= PID_DT_MS) { t3 = now; move_j3(-1400); }
-  if (now - t4 >= PID_DT_MS) { t4 = now; move_j4(800); }
+  
 }
