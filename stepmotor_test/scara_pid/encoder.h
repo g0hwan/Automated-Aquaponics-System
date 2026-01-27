819836ca53117b2f3440fc790f7131de997991c5
thumb1 <lthsty79@gmail.com>
2026-01-27 19:12:39 +0900

    01.27 19:12 기구학 수정 중

--- stepmotor_test/scara_pid/encoder.h
+++ stepmotor_test/scara_pid/encoder.h
@@ -39,20 +39,23 @@ float encoder_getAngleDeg(const encod* e);
 void set_tim();
 void set_int();
 
-void j1stepPulse();
-void j3stepPulse();
-void j4stepPulse();
-
-void j1EncoderA();
-void j3EncoderA();
-void j4EncoderA();
 void set_int();
-
-void move_j1(float targetAngle);   
-
+///////////////////////////////////////////////////////////////////////////////////
+void j1stepPulse();
+void j1EncoderA();
+bool move_j1(float targetAngle, float tolDeg = 1.0f);   
+void move_j1_wait(float targetAngle,
+                  float tolDeg = 1.0f,
+                  unsigned long stable_ms = 150,
+                  unsigned long timeout_ms = 8000);
+void enc_reset_j1();
+void j1_home_stop_on_switch(bool dir_to_switch, unsigned long pps);
+///////////////////////////////////////////////////////////////////////////////////
 bool move_j2(float deg, unsigned long pps);
 void j2EndstopISR();
-
+///////////////////////////////////////////////////////////////////////////////////
+void j3stepPulse();
+void j3EncoderA();
 bool move_j3(float targetAngle, float tolDeg = 1.0f);
 void move_j3_wait(float targetAngle,
                   float tolDeg = 1.0f,
@@ -60,8 +63,9 @@ void move_j3_wait(float targetAngle,
                   unsigned long timeout_ms = 8000);
 void enc_reset_j3();
 void j3_home_stop_on_switch(bool dir_to_switch, unsigned long pps);
-
-
+///////////////////////////////////////////////////////////////////////////////////
+void j4stepPulse();
+void j4EncoderA();
 void move_j4_wait(float targetAngle,
                   float tolDeg = 1.0f,
                   unsigned long stable_ms = 150,
@@ -69,12 +73,8 @@ void move_j4_wait(float targetAngle,
 
 bool move_j4(float targetAngle, float tolDeg = 1.0f);
 void enc_reset_j4();
-void j4_home_stop_on_switch(bool dir_to_switch, unsigned long pps);
-
+bool j4_home_stop_on_switch_safe(bool dir_to_switch, unsigned long pps);
+///////////////////////////////////////////////////////////////////////////////////
 
 void enc_reset_all();          
-void enc_reset_j1();
-
-
-
 #endif
